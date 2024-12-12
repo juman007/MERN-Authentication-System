fc5b7b93c2f07e590e8061d39677dc07f7ed8e73
Juman Saikia <jumansaikia10@gmail.com>
2024-12-12 15:16:19 +0530

    add user Schema

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongoDB.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ Credential: true }));
 
-app.get('/', (req, res) => { 
-    res.send("API WORKING!");
-})
+app.get("/", (req, res) => {
+   res.send("API WORKING!");
+});
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
