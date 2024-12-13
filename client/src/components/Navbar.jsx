659c9e32bed5b491153be7f613fd501f28b5c880
Juman Saikia <jumansaikia10@gmail.com>
2024-12-13 15:01:42 +0530

    navbar and home page complate

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { assets } from "../assets/assets";
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+   return (
+      <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
+         <img className="w-28 sm:w-32" src={assets.logo} alt="" />
+         <button className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all">
+            Login <img src={assets.arrow_icon} alt="" />
+         </button>
+      </div>
+   );
+};
 
-export default Navbar
+export default Navbar;
