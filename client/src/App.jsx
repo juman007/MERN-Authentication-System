72cbf65cd65ffabc7e3ee0f85888c9bdd2cc78dd
Juman Saikia <jumansaikia10@gmail.com>
2024-12-14 14:13:54 +0530

    login, logout api integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,13 @@ import Login from "./pages/Login";
 import EmailVerify from "./pages/EmailVerify";
 import ResetPassword from "./pages/ResetPassword";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
    return (
       <div>
+         <ToastContainer />
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
